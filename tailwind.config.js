12c531b17e346ae718f5764c62fbfea848e06d7f
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2025-05-05 14:22:38 +0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      keyframes: {
-        typing: {
-          "0%": { width: "0ch" },
-          "100%": { width: "29ch" }, // Adjust to match the length of your text
-        },
-        blink: {
-          "0%, 100%": { borderColor: "transparent" },
-          "50%": { borderColor: "black" },
-        },
-      },
-      animation: {
-        typing: "typing 3s steps(20) forwards, blink 0.7s step-end infinite",
-      },
-    },
-  },
   plugins: [],
 };
